c60ad2c03da169ec3c0e4b337ee9c80d442ef5a7
ogaoga <tsutomu.ogasawara+github@gmail.com>
2023-08-12 16:09:39 +0900

    Fix to apply to JSON format

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,40 +1,47 @@
-import { TodoItem } from 'app/page';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+
+type TodoItemJsonData = {
+  id: string;                 // UUID
+  taskName: string;
+  priority: number | null;
+  deadline: string | null;    // ISO 8601 String
+  completed: boolean;
+  completedAt: string | null, // ISO 8601 String
+}
 
 // Initial Todo Items
-const initialItems: TodoItem[] = [
+const initialItems: TodoItemJsonData[] = [
   {
-    id: '58bfad23-3268-4034-a556-3a408f6ed890',
-    taskName: '掃除をする',
+    id: "58bfad23-3268-4034-a556-3a408f6ed890",
+    taskName: "掃除をする",
     priority: 1,
-    deadline: new Date('2020-07-23'),
+    deadline: "2020-07-23T00:00:00.000Z",
     completed: false,
-    // 修正点：TSではundefinedに統一
-    completedAt: undefined,
+    completedAt: null,
   },
   {
-    id: '040222b6-5fb9-4c0d-9bb3-ac2de103fb44',
-    taskName: '植物に水を上げる',
+    id: "040222b6-5fb9-4c0d-9bb3-ac2de103fb44",
+    taskName: "植物に水を上げる",
     priority: 2,
-    deadline: new Date('2020-07-30'),
+    deadline: "2020-07-30T00:00:00.000Z",
     completed: false,
-    completedAt: undefined,
+    completedAt: null,
   },
   {
-    id: '06b12423-5949-46b4-8a3e-fa0552d5a7c4',
-    taskName: '買い物をする',
-    priority: undefined,
-    deadline: undefined,
+    id: "06b12423-5949-46b4-8a3e-fa0552d5a7c4",
+    taskName: "買い物をする",
+    priority: null,
+    deadline: null,
     completed: true,
-    completedAt: new Date('2020-07-10'),
+    completedAt: "2020-07-10T00:00:00.000Z",
   },
   {
-    id: '72a46246-cc3a-4e5e-b3e7-01b0dffcbb4d',
-    taskName: '犬の散歩をする',
-    priority: undefined,
-    deadline: undefined,
+    id: "72a46246-cc3a-4e5e-b3e7-01b0dffcbb4d",
+    taskName: "犬の散歩をする",
+    priority: null,
+    deadline: null,
     completed: true,
-    completedAt: new Date('2020-06-29'),
+    completedAt: "2020-06-29T00:00:00.000Z",
   },
 ];
 
